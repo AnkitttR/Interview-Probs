fd8db5b02a9533759f2795e878f4c94043bc9457
Ankit <ankitthakur1998@gmail.com>
2022-04-07 21:23:44 +0530

    second item in array

---
+++ SeconItem.java
@@ -0,0 +1,14 @@
+public class SeconItem {
+    
+public static void main(String[] args) {
+    
+// Print the second item in the cars array.
+
+String[] cars = {"Volvo", "BMW", "Ford"};
+
+System.out.println(cars[1]);
+
+}
+
+
+}
