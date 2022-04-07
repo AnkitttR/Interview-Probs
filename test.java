b707f6a1ea17551abeccb384f70a1611fb0c615f
Ankit <ankitthakur1998@gmail.com>
2022-04-07 20:54:41 +0530

    learning for (String i : cars)

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    
+   public static void main(String[] args){
+
+    String[] cars = {"Volvo", "BMW", "Ford"};
+    for (String i : cars) {
+     System.out.println(i);
+   }}
+}
