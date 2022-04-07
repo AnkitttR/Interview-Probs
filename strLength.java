a5fc01518742b1c9cac4bc6fe279e9768602af00
Ankit <ankitthakur1998@gmail.com>
2022-04-07 21:31:22 +0530

    Find length of string array

---
+++ strLength.java
@@ -0,0 +1,14 @@
+public class strLength {
+
+public static void main(String[] args) {
+    
+// Find the length of string array
+
+String[] cars = {"Volvo", "BMW", "Ford"};
+
+System.out.println(cars.length);
+
+}
+
+    
+}
