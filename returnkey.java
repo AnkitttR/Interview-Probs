c86de98ade765417fd2fbd194e406dd737db57b6
Ankit <ankitthakur1998@gmail.com>
2022-04-08 17:18:00 +0530

    Use of return keyword

---
+++ returnkey.java
@@ -0,0 +1,16 @@
+public class returnkey {
+    
+// Insert the missing part to print the number 8 in main, by using a specific keyword inside myMethod:
+
+static int myMethod(int x) {
+  
+    return 5 + x;
+    }
+    
+public static void main(String[] args) {
+  System.out.println(myMethod(3));
+}
+
+
+
+}
