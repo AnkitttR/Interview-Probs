04e3fd55d88f581f237950f27f25307f16e412da
Ankit <ankitthakur1998@gmail.com>
2022-04-08 17:38:52 +0530

    checkAge method

---
+++ checkAgemethod.java
@@ -0,0 +1,30 @@
+// Follow the comments to insert the missing parts of the code below:
+
+public class checkAgemethod {
+    
+// Create a checkAge() method with an integer variable called age
+
+static void checkAge(int age){
+
+    // Follow the comments to insert the missing parts of the code below:
+    if (age < 18){
+        System.out.println("Access Denied");
+
+        // // If age is greater than, or equal to, 18, print "Access granted"
+    }else {
+
+        System.out.println("Access granted");
+    }
+}
+
+
+public static void main(String[] args) {
+    
+    // Call the checkAge method and pass along an age of 20
+    checkAge(16);
+}
+
+
+
+
+}
