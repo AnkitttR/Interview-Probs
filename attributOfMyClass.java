577c449276b75aa8d1ffed7e23f4df5d5419087d
Ankit <ankitthakur1998@gmail.com>
2022-04-08 18:03:25 +0530

    Accesing attribute of class through obj

---
+++ attributOfMyClass.java
@@ -0,0 +1,15 @@
+public class attributOfMyClass {
+
+    // Use myObj to access and print the value of the x attribute of MyClass.
+    int x=5;
+    public static void main(String[] args) {
+
+        attributOfMyClass myObj = new attributOfMyClass();
+        System.out.println(myObj.x);
+
+        
+    }
+
+
+    
+}
