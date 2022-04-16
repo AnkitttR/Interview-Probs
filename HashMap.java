bb650046a5c97a6f5cc7bc68566fdbc96f7b30df
Ankit <ankitthakur1998@gmail.com>
2022-04-16 17:29:05 +0530

    HasbhMap object capitalCities created

---
+++ HashMap.java
@@ -0,0 +1,18 @@
+// Import the HashMap class
+import java.util.HashMap;
+
+public class Main{
+public static void main(String[] args){
+//Create a Hashmap object called capitalCities
+HashMap<String,String> capitalCities = new HashMap<String,String>();
+
+
+
+}
+
+
+
+
+
+
+}
